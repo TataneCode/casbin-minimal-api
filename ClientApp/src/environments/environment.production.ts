4fdb6c1792ef31899948be344bee29c3b7163a01
TataneCode <sylvain.turpault@pm.me>
2025-10-27 21:01:41 +0100

    Add production build

---
+++ ClientApp/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+// Base environment (development by default)
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://prod.casbin:9443/api' // Adjust port/host to your backend if different
+};
